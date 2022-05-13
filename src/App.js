131c305c356698543e53d838dd72618b2faf9cbf
khapusova <diana.hapusova@gmail.com>
2022-05-13 23:22:50 +0300

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react';
+import './App.css'
+// import CatsBlock from './components/CatsBlock';
 
-function App() {
+const  App = () => {
+  let [cats, setCats] = useState([]);
+  
+  useEffect(()=>{
+    console.log(cats)
+  },[cats]);
+
+  const refreshPage = ()=>{
+    window.location.reload();
+ }
+
+  const addCat = async () =>{
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const responseJSON = await response.json();
+    setCats(prev=>prev.concat(responseJSON[0])); 
+  }
+
+  const changeCat = async (id) => {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const responseJSON = await response.json();
+    setCats(cats.map(cat=> cat.id===id?responseJSON[0]:cat));
+  }
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header onClick={refreshPage}>
+        <button className='leftside' >😸</button>
+        <h1>Котикогенератор</h1>
       </header>
+
+      <div className='add-cat-btn'>
+      <button onClick={addCat}> Додати котика </button>
+      <div className='cats'>
+        {cats.map(cat => 
+        <div key={cat.id} >
+        <img
+          onClick={()=>changeCat(cat.id)}
+          src={cat.url}
+          alt={cat.description}
+          />
+         <p> {cat.description} </p>
+         </div>
+          )}
+      </div>
+     
+
+      </div>
     </div>
   );
 }
